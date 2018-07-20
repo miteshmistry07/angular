08cd16b23eefdc1b97278be67636ae87f9a99ca7
Mitesh Mistry <mistrymitesh@hotmail.com>
2018-07-20 14:20:02 +0100

    end of components

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  //by attribute selector: '[app-servers]', 
+  //by class selector: '.app-servers',
   selector: 'app-servers',
   template: `
     <app-server></app-server>
-    <app-server></app-server>`  
+    <app-server></app-server>`,  
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
