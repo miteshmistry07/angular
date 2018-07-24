1a943546311ccb5edb54df6f65ef9552798a5451
Mitesh Mistry <mistrymitesh@hotmail.com>
2018-07-24 14:06:12 +0100

    data, events binding exercise completed

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,16 +4,30 @@ import { Component, OnInit } from '@angular/core';
   //by attribute selector: '[app-servers]', 
   //by class selector: '.app-servers',
   selector: 'app-servers',
-  template: `
+ /* template: `
     <app-server></app-server>
-    <app-server></app-server>`,  
+    <app-server></app-server>`,   */
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'testserver';
+
+  constructor() {
+    setTimeout(()=>{this.allowNewServer = true;}, 2000);
+   }
 
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
